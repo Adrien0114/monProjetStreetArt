ef6cae579614f6735dc9e1a833a738fd0479e2ca
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-30 22:58:51 +0200

    Amélioration css

--- imports/ui/profil/profil.js
+++ imports/ui/profil/profil.js
@@ -1,6 +1,8 @@
 import './profil.html';
 import './profil.css';
 
+import { Meteor } from 'meteor/meteor';
+
 Template.profil.helpers({
     utilisateur_prenom: () => Meteor.user().profile.first_name,
     utilisateur_nom: () => Meteor.user().profile.last_name,
