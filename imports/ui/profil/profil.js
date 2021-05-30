3afb24f6e6a07dd28f4960e63ac49ebfaf3bde6b
Andrei Anikin <59820123+Andrei-ctrl@users.noreply.github.com>
2021-05-30 21:52:37 +0200

    Ajout CSS sur toutes les pages
    Co-Authored-By: Klinsmann Kodra <37582939+klinsmannkodra@users.noreply.github.com>

--- imports/ui/profil/profil.js
+++ imports/ui/profil/profil.js
@@ -2,10 +2,10 @@ import './profil.html';
 import './profil.css';
 
 Template.profil.helpers({
-    utilisateur: () => Meteor.user().username,
+    utilisateur_prenom: () => Meteor.user().profile.first_name,
+    utilisateur_nom: () => Meteor.user().profile.last_name,
     email: () => Meteor.user().emails[0].address,
 })
-
 Template.profil.events({
     'click #logout'(event){
     event.preventDefault();
