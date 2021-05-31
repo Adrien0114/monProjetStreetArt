019cf033f86fab977d425d5f300d3420cc813afd
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-31 08:53:07 +0200

    Indentation

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -1,7 +1,5 @@
 import './body.html';
 import './body.css';
-//import '../collection/collections.js';
-//import '../element_collection/element_collection.js';
 import '../../api/maps/maps-geoloc.js';
 import '../../../lib/routing.js';
 import '../accueil/accueil.js';
@@ -18,15 +16,15 @@ import '../afficherParcours/afficherParcours.js';
 import { Session } from 'meteor/session';
 
 Template.app_body.events({
-    'click #cta'(event){
+  'click #cta'(event) {
     event.preventDefault();
     let routeActuelle = FlowRouter.getRouteName();
-    if (routeActuelle == "afficherParcours") {
-        const param = FlowRouter.getParam('_parcoursId');
-        routeActuelle = [routeActuelle, param]
+    if (routeActuelle == 'afficherParcours') {
+      const param = FlowRouter.getParam('_parcoursId');
+      routeActuelle = [routeActuelle, param];
     }
     Session.set('routeDOrigine', routeActuelle);
     console.log(Session.get('routeDOrigine'));
     FlowRouter.go('descriptif');
-    }
-})
+  },
+});
